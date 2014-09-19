bb468c6783085cd5ff2db66914c7486641cb17f4
hongelai <hongelai@gmail.com>
2014-09-19 15:37:06 -0700

    single Number

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,16 @@
 #include <vector>
 #include <list>
 #include <string>
+
 using namespace std;
+
+
 
 int main ()
 {
-	string s = "adss";
-	reverseWords(s);
-    cout<<s;
+	int array [7] = {2,4,1,3,3,1,2};
+	cout<<singleNumber(array,7)<<endl;
+
 	system("pause");
 	return 0;
 }
---
+++ singleNumber.cpp
@@ -0,0 +1,6 @@
+int singleNumber(int A[], int n) {
+	int single = 0;
+	for(int i = 0; i < n; i++)
+		single ^= A[i];
+	return single;
+}
