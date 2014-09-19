dd21a6655ccf746c413ca5005749e56dd2221dcb
hongelai <hongelai@gmail.com>
2014-09-19 14:15:10 -0700

    reverse word

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+// queue::push/pop
+#include <iostream>
+#include <queue>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+int main ()
+{
+	string s = "adss";
+	reverseWords(s);
+    cout<<s;
+	system("pause");
+	return 0;
+}
