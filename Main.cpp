e003b4ef501fe1d81d02aa73f3f59f92ac33f1d1
hongelai <hongelai@gmail.com>
2014-10-16 02:32:11 -0700

    mm

--- Main.cpp
+++ Main.cpp
@@ -12,17 +12,16 @@
 #include <set>
 #include <algorithm>
 #include <stack>
-#include "LargestRectangleinHistogram.h"
-using namespace std;
+#include <sstream>
 
+using namespace std;
 
 int main ()
 {
-	int array[]={2,1,5,6,2,3};
-	vector<int> num(array,array+6);
-	
-	cout<<largestRectangleArea(num)<<endl;
-
+	int a[]={1,3,4,33,11,22,33,42,12,1};
+    vector<int> v(a,a+10);
+    function<bool(int,int)> less_than = [](int a,int b){return a<b;};
+    cout<<count_if(v.begin(),v.end(),bind1st(less_than,20))<<endl;
 
 	return 0;
 }
