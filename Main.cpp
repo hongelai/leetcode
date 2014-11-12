d119324c3b7c613428b81b209ec693cc8807dd6f
hongelai <hongelai@gmail.com>
2014-11-11 16:20:18 -0800

    mm

--- Main.cpp
+++ Main.cpp
@@ -11,33 +11,43 @@
 #include <stack>
 #include <sstream>
 #include <regex>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
+int findFirstUnique(vector<int> &v){
 
-bool isNumber(const char *s) {
-        //regex e("(\\s)*[+-]?((\\.[0-9]+)|([0-9]+\\.[0-9]?)([eE][+-]?[0-9]+)?(\\s)*");
-        regex e("(\\s*)[+-]?((\\.[0-9]+)|([0-9]+(\\.[0-9]*)?))(e[+-]?[0-9]+)?(\\s*)");
-        if(regex_match("2e10",e))
-            return true;
-        else return false;
-    }
-int maxProfit(vector<int> &prices) {
-    if (prices.size() < 2) return 0;
-        int profit = 0; // 差价，也就是利润
-        int cur_min = prices[0]; // 当前最小
-        for (int i = 1; i < prices.size(); i++) {
-            profit = max(profit, prices[i] - cur_min);
-            cur_min = min(cur_min, prices[i]);
-        }
-    return profit;
+	unordered_map<int,int> um;
+	unordered_set<int> us;
+	for(int i = 0; i < v.size(); i++){
+		if(!um[v[i]]){
+			// um.insert(make_pair(a,1)); // cannot use insert again(duplicate), because !um[v[i]] will innitialize it as 0.
+			um[v[i]] = 1;
+			us.insert(v[i]);
+		}
+		else{
+			um[v[i]]++;
+			us.erase(v[i]);
+		}
+	}
+	// for(auto it = um.begin();it!=um.end();it++) cout<<it->first<<" "<<it->second<<endl;
+	for(auto it = us.begin();it!=us.end();it++) cout<<*it<<" ";
+
+	return *us.begin();
 }
+
 int main ()
 {
 
-	int array[]={3,1,2,3,4,5,1,2,10,3};
+	int array[]={1,2,2,3,4,3,1,5,3,11};
 	vector<int> v(array,array+10);
 
-    cout<<maxProfit(v)<<endl;
-	system("pause");
-
+	cout<<findFirstUnique(v)<<endl;
+	// unordered_map<int,pair<int,int> > um;
+	// um.insert(make_pair(1,make_pair(2,3)));
+	// um.insert(make_pair(2,make_pair(32,4)));
+	// um.insert(make_pair(3,make_pair(21,22)));
+	// auto it = um.begin();
+	// for(;it!=um.end();it++)
+	// 	cout<<(it->second).first<<"  ";
 	return 0;
 }
