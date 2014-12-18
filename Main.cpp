14e2a6e18d3e9c6def232ed6c6b78bf2d4b85882
hongelai <hongelai@gmail.com>
2014-12-17 18:44:39 -0800

    restore IP address

--- Main.cpp
+++ Main.cpp
@@ -36,47 +36,15 @@ int findFirstUnique(vector<int> &v){
 
 	return *us.begin();
 }
-    string minWindow(string S, string T) {
-        int M = S.size(),N = T.size();
-        if(M < N) return "";
-        int need[256] = {0}, find[256] = {0};
-        
-        int start, end,rstart = -1,rend = M,count=0;
-        start = end = 0;
-        
-        for(int i = 0; i < N; i++) need[T[i]]++;
 
-        for(int i = 0; i < M; i++,end++){
-            if(!need[S[i]]) continue;
-            
-            find[S[i]]++;
-            if(find[S[i]] <= need[S[i]]) count++;
-            if(count == N){ //find all needed characters
-                for(;start <= end; start++){
-                    if(!find[S[start]]) continue;
-                    if(find[S[start]] <= need[S[start]]) break;
-                    find[S[start]]--;
-                }
-                //update rstart , rend
-                if(end- start < rend - rstart){
-                    rstart = start;
-                    rend = end;
-                }
-            }
-            
-             
-        }
-        cout<<rstart<<" "<<rend<<endl;
-        return rstart == -1? "":string(S.begin()+rstart,S.begin()+rend+1);
-    }
 
 int main ()
 {
     int aa[]={1,1,1,0};
     
     vector<int> vc(aa,aa+4);
-    int a = 1 <<2 -1;
-    cout<<a<<endl;
+    vector<string> dic = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    cout<<dic[2].size()<<endl;
 
 	system("pause");
 
