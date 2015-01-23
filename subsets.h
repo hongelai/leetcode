ae7e7801e86f008a44d3b901f6ca8adb3035b524
hongelai <hongelai@gmail.com>
2015-01-23 11:38:32 -0800

    subsets.h

--- subsets.h
+++ subsets.h
@@ -1,3 +1,4 @@
+    //back  tracking
     vector<vector<int> > subsets(vector<int> &S) {
         vector<vector<int> > res;
         vector<int> entry;
@@ -16,4 +17,25 @@
             dfs(s,i+1,entry,res);
             entry.pop_back();
         }
+    }
+
+
+    //iterative
+        vector<vector<int> > subsets(vector<int> &S) {
+        int size = S.size();
+        vector<vector<int> > res;
+        vector<int> entry;
+        res.push_back(entry);
+        
+        sort(S.begin(),S.end());
+        for(int i = 0; i < size; i++){//1; 2; 1,2; 3;1,3;2,3;1,2,3;
+            int sz = res.size();
+            
+            for(int j = 0; j < sz; j++){
+                entry = res[j];
+                entry.push_back(S[i]);
+                res.push_back(entry);
+            }
+        }
+        return res;
     }
