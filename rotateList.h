0071f7629aa8c3e57da8edb62ac8a189b49e918f
hongelai <hongelai@gmail.com>
2015-01-25 12:02:46 -0800

    rotate list

--- rotateList.h
+++ rotateList.h
@@ -1,8 +1,6 @@
     ListNode *rotateRight(ListNode *head, int k) {
         if(!head || !head->next) return head;
-        
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = head;
+
         ListNode *fast, * slow;
         slow = fast = head;
         for(int i = 0; i < k; i++){
@@ -13,16 +11,9 @@
             slow = slow->next;
             fast = fast->next;
         }
-
-        ListNode *pre, *post;
-        pre = slow; post = dummy;
-        
-        while(pre && pre->next){
-            ListNode* tmp = pre->next;
-            pre->next = pre->next->next;
-            tmp->next = post->next;
-            post->next = tmp;
-            post = post->next;
-        }
-        return dummy->next;
+        //cut after slow pointer head...slow, ....fast->NULL
+        fast->next = head;
+        head = slow->next;
+        slow->next = NULL;
+        return head;
     }
