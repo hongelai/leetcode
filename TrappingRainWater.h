298d335aaba0ae8ed7b5b436e6ad3aef7df623a9
hongelai <hongelai@gmail.com>
2015-02-05 23:24:03 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Main.cpp

--- TrappingRainWater.h
+++ TrappingRainWater.h
@@ -13,3 +13,24 @@
 
         return res;
     }
+
+    int trap_2(int A[], int n) {
+        if (A == NULL || n <= 2) return 0;
+        int left= 1;
+        int right = n - 2;
+        int maxLeft = A[0];
+        int maxRight = A[n - 1];
+        int res = 0;
+        while (left <= right) {  //因为最终的计算还是以最短板为准，所以可以不用存起来所有结果，而是每次处理短板一边的。
+            if (maxLeft <= maxRight) {
+                res += max(0, maxLeft - A[left]);
+                maxLeft = max(maxLeft, A[left]);
+                ++left;
+            } else {
+                res += max(0, maxRight - A[right]);
+                maxRight = max(maxRight, A[right]);
+                --right;
+            }
+        }
+        return res;
+    }
