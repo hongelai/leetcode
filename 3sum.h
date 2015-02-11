bd909a8c13d7b4d8369d9f6de777e55e67546e98
hongelai <hongelai@gmail.com>
2015-02-11 11:48:00 -0800

    rename

--- 3sum.h
+++ 3sum.h
@@ -1,34 +1,4 @@
    //k-SUM 的解法 先固定其中的k-2个值，剩下的2个值动态查找匹配的，最优解永远是O(n^k-2)
-
-    vector<vector<int> > threeSum(vector<int> &num) {
-        vector<vector<int> > res;
-        if(num.size() < 3) return res;
-        sort(num.begin(),num.end());    //排序后可以夹逼求值
-        int target = 0;
-        
-        for(int i = 0; i < num.size()-2; i++){
-            if(n > 0 && num[i] == num[i-1]) continue;   //跳过，例如-1，-1，-1,0,1
-            
-            int pre = i+1, last = num.size()-1;
-
-            while(pre < last){
-                if(num[i]+num[pre]+num[last] > target){
-                    last--;
-                }else if(num[i]+num[pre]+num[last] < target){
-                    pre++;
-                }else{
-                    res.push_back({num[i],num[pre],num[last]});
-
-                    //对于给定的num[i],找到了一个值后，继续在原来的基础上缩小范围查找
-                    if(num[last] != num[last-1])  last--; 
-                    if(num[pre] != num[pre+1])  pre++;
-                }
-            }
-            
-        }
-        return res;
-    }
-
     //这是边跑边去重
     vector<vector<int> > threeSum(vector<int> &num) {
         vector<vector<int> > res;
