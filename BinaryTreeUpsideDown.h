b107f9e96052969364cd7ffb2842c86e9ee1cc73
hongelai <hongelai@gmail.com>
2015-02-12 15:54:50 -0800

    bianry tree  up side down

--- BinaryTreeUpsideDown.h
+++ BinaryTreeUpsideDown.h
@@ -1,12 +1,12 @@
 public TreeNode UpsideDownBinaryTree(TreeNode root) {
    TreeNode p = root, parent = null, parentRight = null;0 d  ^4 s  P3 O1 J3 {
    while (p != null) {
-      TreeNode left = p.left;
-      p.left = parentRight;
-      parentRight = p.right;
-      p.right = parent;
-      parent = p;
-	  p = left; 
+      TreeNode left = p.left;//把当前节点的左节点作为下个节点保存下来作为next指针，接下来把p和p.right单独提取出来赋值构建树
+      p.left = parentRight;   //把原始的树中p的父节点的右节点作为p的新左节点
+      parentRight = p.right;  //把p.right保存下来给后面节点用（p->next）
+      p.right = parent;       //把p的原始树中的parent作为p的右节点
+      parent = p;             //update p 作为parent
+	  p = left;                //move on to next
 	}
    	return parent;
 }
@@ -17,7 +17,7 @@ public TreeNode UpsideDownBinaryTree(TreeNode root) {
 private TreeNode dfsBottomUp(TreeNode p, TreeNode parent) {
    if (p == null) return parent;
    TreeNode root = dfsBottomUp(p.left, p);
-   p.left = (parent == null) ? parent : parent.right;
+   p.left = (parent == null) ? parent : parent.right; //把parent 和 parent的右节点 作为孩子， 返回上一级再去取parent的parent和和parent.right作为子节点
    p.right = parent;
-   return root;
+   return root;//最后一个节点就是根节点，所以一路返回
 }
