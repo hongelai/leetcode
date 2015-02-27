2318d7e6b789af1513bdb62b31a8d83609967ee8
hongelai <hongelai@gmail.com>
2015-02-26 23:46:08 -0800

    update

--- PathSum.h
+++ PathSum.h
@@ -1,12 +1,28 @@
-bool hasPathSum(TreeNode *root, int sum) {
-    bool leftResult=false,rightResult = false;
-    if(root == NULL) return false;
-    if(root->left == NULL && root-> right == NULL)
-        if(root->val == sum)return true;
-        else return false;
-    if(root->left != NULL)
-        leftResult = hasPathSum(root->left,sum - root->val);
-    if(root->right != NULL)
-        rightResult = hasPathSum(root->right, sum - root->val);
-    return (leftResult || rightResult);
-}
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(root == NULL) return false;
+        bool hasOne = false;
+        if(!root->left && !root->right) return root->val == sum ? true : false;
+        if(root->left) hasOne |= hasPathSum(root->left, sum - root->val);
+        if(root->right) hasOne |= hasPathSum(root->right, sum - root->val);
+        return  hasOne;
+    }
+
+    //iterative
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(root == NULL) return false;
+        queue<pair<TreeNode*,int> > q;
+        q.push(make_pair(root, sum - root->val));
+        
+        while(!q.empty()) {
+            TreeNode *tmp = q.front().first;
+            int leftSum = q.front().second;
+            q.pop();
+            if(!tmp->left && !tmp->right){
+                if(leftSum == 0) return true;
+                else continue;
+            }
+            if(tmp->left) q.push(make_pair(tmp->left, leftSum - tmp->left->val));
+            if(tmp->right) q.push(make_pair(tmp->right, leftSum - tmp->right->val));
+        }
+        return false;
+    }
