4cce131483bc3a2d4cae448c4fe4430f910bd1ef
hongelai <hongelai@gmail.com>
2015-04-12 22:20:09 -0700

    update

--- longestPalindrome.h
+++ longestPalindrome.h
@@ -1,19 +1,14 @@
     string longestPalindrome(string s) {
-        int length = s.length();
-        // vector<vector<bool> > dp(length,vector<bool>(length)); // time limit exceeded
-        bool dp[1000][1000] = {false};
-        string res= "";
-        int max = 0;
-        int start = 0, end = length -1;
+        int len = s.length();
+        string res = "";
+        bool dp[len][len] = {false};
         
-        for(int i = length - 1 ; i >= 0; i--)
-            for(int j = i; j < length; j++){
-                dp[i][j] = (s[i]==s[j]) && (i+2 > j || dp[i+1][j-1]);
-                if(dp[i][j] && j - i >= max){
-                    max = j - i + 1;
-                    start = i;
-                    end = j;
+        for(int i = len-1; i >= 0; i--)
+            for(int j = i; j < len; j++){
+                dp[i][j] = s[i] == s[j] && (j-i < 2 || dp[i+1][j-1]);
+                if(dp[i][j] && j - i + 1 > res.length()){
+                    res = s.substr(i,j-i+1);
                 }
             }
-        return s.substr(start,end-start+1);
+        return res;
     }
--- reorderList.h
+++ reorderList.h
@@ -1,46 +1,31 @@
     void reorderList(ListNode *head) {
-        //find the mid node, cut the list into two from half and reverse. And then combine
         if(head == NULL || head->next == NULL) return;
-        ListNode* fast, * slow;
-        fast = head->next->next; slow = head;
+        ListNode *fast, *slow;
+        slow = head, fast = head->next;
         
-        //find mid
-        while(fast){
+        while(fast && fast->next){
             slow = slow->next;
-            if(fast->next && fast->next->next) fast = fast->next->next;
-            else break;
+            fast = fast->next->next;
         }
         
-        //cut list into two, slow....., fast.....
+        //reverse second half
+        fast = slow->next;
+        while(fast->next){
+            ListNode* tmp = fast->next;
+            fast->next = fast->next->next;
+            tmp->next = slow->next;
+            slow->next = tmp;
+        }
+        
+        //seperate and then merge 
         fast = slow->next;
         slow->next = NULL;
         slow = head;
-        
-        //reverse second list
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = fast;
-        ListNode *pre;
-        pre = dummy->next;
-        while(pre && pre->next){
-            ListNode *tmp = pre->next;
-            pre->next = pre->next->next;
-            tmp->next = dummy->next;
-            dummy->next = tmp;
-        }
-        fast = dummy->next;
-        
-        //merge two list
-        ListNode * pt = new ListNode(-1);
-        dummy = pt;
-        
-        while(fast && slow){
-            pt->next = slow;
-            slow = slow->next;
-            pt = pt->next;
-            pt->next = fast;
+        while(fast){
+            ListNode* tmp = fast;
             fast = fast->next;
-            pt = pt->next;
+            tmp->next = slow->next;
+            slow->next = tmp;
+            slow = slow->next->next;
         }
-        pt->next = slow;
-        head = dummy->next;
     }
